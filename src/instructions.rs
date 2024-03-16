5c1180e9dcceab92f40e4fde625fd74c9a26fdb2
Juraj Borza <jborza@gmail.com>
2024-03-16 09:52:42 +0100

    more on instructions and jumps

--- src/instructions.rs
+++ src/instructions.rs
@@ -1,5 +1,10 @@
+enum JumpTest{
+    NotZero, Zero, NotCarry, Carry, Always,
+}
+
 enum Instruction { 
-    ADD(ArithmeticTarget)
+    ADD(ArithmeticTarget),
+    JMP(JumpTest),
 }
 
 enum ArithmeticTarget {
@@ -7,11 +12,25 @@ enum ArithmeticTarget {
 }
 
 impl Instruction{
-    fn from_byte(byte: u8) -> Instruction {
+    fn from_byte(byte: u8, prefixed: bool) -> Option<Instruction> {
+        if prefixed {
+          Instruction::from_byte_prefixed(byte)
+        } else {
+          Instruction::from_byte_not_prefixed(byte)
+        }
+    }
+
+    fn from_byte_prefixed(byte: u8) -> Option<Instruction> {
         match byte {
-            0x02 => Some(Instruction::INC(IncDecTarget::BC)),
-            0x13 => Some(Instruction::INC(IncDecTarget::DE)),
-            _ => /* TODO Add mapping for the rest of instructions */ None
+          0x00 => Some(Instruction::RLC(PrefixTarget::B)),
+          _ => /* TODO: Add mapping for rest of instructions */ None
+        }
+    }
+    
+    fn from_byte_not_prefixed(byte: u8) -> Option<Instruction> {
+        match byte {
+          0x02 => Some(Instruction::INC(IncDecTarget::BC)),
+          _ => /* TODO: Add mapping for rest of instructions */ None
         }
     }
 }
